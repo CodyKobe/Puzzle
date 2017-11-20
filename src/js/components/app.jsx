59aef43f4fb5e7b9e873532fd2cb39e6295abd84
CodyKobe <pawel.tutak@outlook.com>
2017-11-20 15:01:11 +0100

    initial commit

---
+++ src/js/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Header} from './header.jsx';
+import {About} from './about.jsx';
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+}
+
+export {App}
