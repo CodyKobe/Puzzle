bf04b8c5f783996a96abaf5f3c3090b2ea7c1841
CodyKobe <pawel.tutak@outlook.com>
2017-11-29 14:58:42 +0100

    commented unused code

--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {Header} from './header.jsx';
 import {Board}  from './board.jsx';
 import {Footer} from './footer.jsx';
-import {About} from './about.jsx';
+// import {About} from './about.jsx';
 import {myImage} from './tile.jsx';
 
 class App extends React.Component {
